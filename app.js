3dd9ad7d36dbdb25269a5718a34aff6c15c5aaf1
biancajoseph101 <biancajoseph101@gmail.com>
2021-11-19 10:31:25 -0600

    added compare cards function

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 //variables
-const cards = [...document.querySelectorAll('.card')];
+const cards = [...document.querySelectorAll('.backcard')];
 const display = document.querySelector('.display');
 const reset = document.querySelector('#resetButton');
 
-let flipCard = [];
-let gameOver = false;
+const cardBack =
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2017%2F03%2F28%2F434817-raccoons-wildlife-animals.jpg&f=1&nofb=1';
 
-let cardObjects = [
+//const flippedCards =
+let flipOne = {
+  name: '',
+  index: ''
+};
+let flipTwo = {
+  name: '',
+  index: ''
+};
+let cardsMatched = false;
+//let gameOver = false;
+
+const cardObjects = [
   {
-    name: 'green frog1',
+    name: 'schoolFish',
+    card: 0,
+    url: "http://bwvp.ecolinc.vic.edu.au/sites/default/files/species/Verreaux's_Frog_01_0.jpg"
+  },
+  {
+    name: 'schoolFish',
     card: 1,
     url: "http://bwvp.ecolinc.vic.edu.au/sites/default/files/species/Verreaux's_Frog_01_0.jpg"
   },
   {
-    name: 'green frog2',
-    card: 2,
-    url: "http://bwvp.ecolinc.vic.edu.au/sites/default/files/species/Verreaux's_Frog_01_0.jpg"
-  },
-  {
     name: 'blue frog1',
-    card: 3,
+    card: 2,
     url: 'https://cdn.shopify.com/s/files/1/0341/4893/products/azureus_1024x1024.jpg?v=1576277674'
   },
   {
     name: 'blue frog2',
-    card: 4,
+    card: 3,
     url: 'https://cdn.shopify.com/s/files/1/0341/4893/products/azureus_1024x1024.jpg?v=1576277674'
   }
 ];
@@ -33,19 +45,57 @@ let cardObjects = [
 //   gameOver = true;
 // }
 
-console.log(cardObjects[1].url);
-//functions
-
-//function cardsMatched() {}
+//console.log(cardObjects[1].url);
 
 //event listeners
 //flip cards
 for (let i = 0; i < cards.length; i++) {
-  console.log(cards[i]);
   cards[i].addEventListener('click', () => {
     let cardDiv = cards[i];
     cardDiv.setAttribute('src', cardObjects[i].url);
+    //below is assigning value of card names to flipped cards
+    if (flipOne.name === '') {
+      flipOne.name = cardObjects[i].name;
+      flipOne.index = i;
+    } else {
+      flipTwo.name = cardObjects[i].name;
+      flipTwo.index = i;
+      compareCards();
+    }
   });
+}
+
+//put timer function
+function compareCards() {
+  if (flipOne.name === flipTwo.name) {
+    cardsMatched = true;
+  } else {
+    cardsMatched = false;
+  }
+  console.log(cardsMatched);
+  resetBoard();
+}
+
+function resetBoard() {
+  if (cardsMatched === true) {
+    //other winning stuff
+
+    flipOne.name = '';
+    flipOne.index = '';
+    flipTwo.name = '';
+    flipTwo.index = '';
+  } else {
+    incorrectPair();
+    flipOne.name = '';
+    flipOne.index = '';
+    flipTwo.name = '';
+    flipTwo.index = '';
+  }
+}
+
+function incorrectPair() {
+  cards[flipOne.index].setAttribute('src', cardBack);
+  cards[flipTwo.index].setAttribute('src', cardBack);
 }
 
 // for (let i = 0; i < cards.length; i++) {
@@ -53,19 +103,6 @@ for (let i = 0; i < cards.length; i++) {
 //         if (cardsMatched)
 //     })}
 
-//   let flipCard = cards[i];
-//   cards[i].style.backgroundColor = 'transparent';
-//   ;
-// }
-
-// for (let i = 0; i < cards.length; i++) {
-//   cards[i].addEventListener('click', cardClicked);
-// }
-
-// function cardClicked(){
-//     let cards.style.backgroundColor = 'transparent'
-// }
-
-resetButton.addEventListener('click', resetGameboard);
+//resetButton.addEventListener('click', resetGameboard);
 
 //const resetGameboard =
