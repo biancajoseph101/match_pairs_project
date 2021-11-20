18f1d2f3106d8ea3944b23649b708e680eaa5797
biancajoseph101 <biancajoseph101@gmail.com>
2021-11-19 18:05:16 -0600

    added second html and js

--- app2.js
+++ app2.js
@@ -20,44 +20,44 @@ let gameOver = false;
 
 const cardObjects = [
   {
-    name: 'fishSchool',
+    name: 'hedgehogsArray',
     card: 0,
-    url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-8aNjprT77ok%2FT3R9cs7niMI%2FAAAAAAAADQw%2F7Wx7srfgZkM%2Fs1600%2Fgroup-of-fish-underwater-01.gif&f=1&nofb=1'
+    url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.animalfactsencyclopedia.com%2Fimages%2Fhedgehogs.jpg&f=1&nofb=1'
   },
   {
-    name: 'frogArmy',
+    name: 'crowsMurder',
     card: 1,
-    url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F547%2F757%2Foriginal%2Fmilitary-people-vector.jpg&f=1&nofb=1'
+    url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipground.com%2Fimages%2Fmurder-clipart-3.jpg&f=1&nofb=1'
   },
   {
-    name: 'lionPride',
+    name: 'komodoDragonsBank',
     card: 2,
-    url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.dailymail.co.uk%2Fi%2Fpix%2F2008%2F12%2F01%2Farticle-0-02A57477000005DC-81_468x296.jpg&f=1&nofb=1'
+    url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F1%2FBank-PNG-File.png&f=1&nofb=1'
   },
   {
-    name: 'fishSchool',
+    name: 'komodoDragonsBank',
     card: 3,
-    url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.drodd.com%2Fimages10%2Fschool-clipart20.jpg&f=1&nofb=1'
+    url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.physorg.com%2Fnewman%2Fgfx%2Fnews%2F2014%2Fagroupofkomo.jpg&f=1&nofb=1'
   },
   {
-    name: 'puppiesLitter',
+    name: 'hedgehogsArray',
     card: 4,
-    url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdogsfordisabled.ie%2Fdfd%2Fwp-content%2Fuploads%2F2016%2F01%2Fpuppy-group-retrievers-768x549.jpg&f=1&nofb=1'
+    url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-9x6DNHTc5DM%2FXv8cVsALnFI%2FAAAAAAAAAM8%2F_2r5nNMCaHMVOLyyu8JzsL__aJCaPYPhwCPcBGAYYCw%2Fs1600%2Farrayinc.jpg&f=1&nofb=1'
   },
   {
-    name: 'puppiesLitter',
+    name: 'stingraysFever',
     card: 5,
-    url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipart-library.com%2Fnewhp%2F121-1214141_litter-clipart-doing-thing-littering-clipart-png.png&f=1&nofb=1'
+    url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages2.minutemediacdn.com%2Fimage%2Fupload%2Fc_fill%2Cg_auto%2Ch_1248%2Cw_2220%2Fv1555313838%2Fshape%2Fmentalfloss%2Fstingrayshed.png%3Fitok%3DbZRTy2Cj&f=1&nofb=1'
   },
   {
-    name: 'frogArmy',
+    name: 'crowsMurder',
     card: 6,
-    url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F04%2F33%2F13%2F0433137689a986445228443ce0c714dd.jpg&f=1&nofb=1'
+    url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic01.nyt.com%2Fimages%2F2015%2F10%2F01%2Fscience%2F01ZIMMER%2F01ZIMMER-master1050.jpg&f=1&nofb=1'
   },
   {
-    name: 'lionPride',
+    name: 'stingraysFever',
     card: 7,
-    url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.istockphoto.com%2Fvectors%2Fpride-month-rainbow-flag-symbol-pride-month-event-celebration-on-vector-id1150848503%3Fk%3D6%26m%3D1150848503%26s%3D170667a%26w%3D0%26h%3DEti4l9YLmIwOaJPZqe_uBkZYcYfAT2y-fNGZYcmbwSE%3D&f=1&nofb=1'
+    url: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipartmag.com%2Fimages%2Ffever-clipart-25.jpg&f=1&nofb=1'
   }
 ];
 
@@ -101,7 +101,6 @@ function cardListener(i) {
       setTimeout(compareCards, 1500);
       break;
     default:
-      console.log('something has gone horribly wrong help');
   }
 }
 
