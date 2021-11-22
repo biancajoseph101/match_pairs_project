ee6de84d250933b699fbadeae4fd6d25f8019137
biancajoseph101 <biancajoseph101@gmail.com>
2021-11-22 10:25:48 -0600

    added buttons to homepage

---
+++ homepage.js
@@ -0,0 +1,17 @@
+const firstLevelButton = (document.querySelector('#firstLevelButton').onclick =
+  function () {
+    location.href =
+      'file:///Users/bianca/ga_seir/projects/match_pairs_project/index.html';
+  });
+
+const nextLevelButton = (document.querySelector('#nextLevelButton').onclick =
+  function () {
+    location.href =
+      'file:///Users/bianca/ga_seir/projects/match_pairs_project/index2.html';
+  });
+
+const lastLevelButton = (document.querySelector('#lastLevelButton').onclick =
+  function () {
+    location.href =
+      'file:///Users/bianca/ga_seir/projects/match_pairs_project/index3.html';
+  });
